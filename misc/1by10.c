d38e6db5aaa869276e40939e5263ad113d6cfd3f
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2024-04-06 12:36:13 +0530

    Initial commit
    Signed-off-by: Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>

---
+++ misc/1by10.c
@@ -0,0 +1,18 @@
+/*
+ * 1by10.c
+ * Dividing 1.0 by 10.0 (floating point) shows the result:
+ *  0.100000000000000005551115123126
+ * clearly demonstrating how the number of digits of precision matters!
+ * (Here, the 55111... portion begins after 18 digits! If the register holding
+ * the result is smaller, it gets truncated. With something like a missile
+ * tracking system, this truncation can be quite literally fatal! ...As was the
+ * case with the Patriot Scud-missile-killer in the Gulf War in 1991, when it missed).
+ */
+#include <stdio.h>
+double res = 0;
+
+int main()
+{
+	res = 1.0/10.0;
+	printf("res of 1/10 is %.30f\n", res);
+}
