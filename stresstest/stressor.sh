15c18ced2affbb682c5d6a1f3043be1cc1d929bd
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2024-04-06 12:36:45 +0530

    Initial commit
    Signed-off-by: Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>

---
+++ stresstest/stressor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# stressor.sh
+# Stress system
+#  with stress(1) : CPU, I/O, VM, disk
+
+# Wrapper over stress(1) and stress-ng(1)
+
+[ $# -ne 1 ] && {
+	echo "Usage: $0 duration-in-seconds (for both IO and n/w)"
+	exit 1
+}
+
+#sudo stress -v --cpu 4 --io 4 --vm 4 --hdd 4 &
+# IO, disk
+echo "1. sudo stress -v --io 8 --hdd 8 --timeout $1"
+sudo stress-ng -v --io 8 --hdd 8 --timeout $1
+# net
+echo "2. sudo stress-ng -v --all 0 --class network ? --timeout 3"
+sudo stress-ng -v --all 0 --class network ? --timeout 3
