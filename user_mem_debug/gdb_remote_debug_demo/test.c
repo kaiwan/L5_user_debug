de6600741678e12cebd7a1ef72ccd92dd856a7ff
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2024-04-14 19:15:31 +0530

    no signal, just sleep; tidy up
    Signed-off-by: Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>

--- user_mem_debug/gdb_remote_debug_demo/test.c
+++ user_mem_debug/gdb_remote_debug_demo/test.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <signal.h>
 
-static void gotit(int sig)
-{
-	printf("%s(): got it!\n", __func__);
-}
-
-int main(void)
+static void do_work(void)
 {
 	int i;
 
-	signal(SIGINT, gotit);
-	printf("PID: %d\nsleep ...\n", getpid());
-	pause();
-//	sleep(5);
-
-	//for (i = 1; i < 10000; i++) {
 	for (i = 1; i < 10; i++) {
 		if (i % 3 == 0)
 			printf("%d is divisible by 3\n", i);
 		if (i % 11 == 0)
 			printf("%d is divisible by 11\n", i);
 	}
+}
 
+int main(void)
+{
+	int dly = 60;
+
+#ifdef DEBUG
+	printf("Debug artifact: PID %d sleeping for %ds now...\n",
+	       getpid(), dly);
+	sleep(dly);
+#endif
+	do_work();
 	return 0;
 }
