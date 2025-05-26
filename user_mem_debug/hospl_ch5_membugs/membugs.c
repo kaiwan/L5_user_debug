f6a8a444bcffd2cdcc76868ab6de3f26cb3e4aa0
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2025-05-26 12:36:28 +0530

    membugs.c: add reqd code from common.h here itself

--- user_mem_debug/hospl_ch5_membugs/membugs.c
+++ user_mem_debug/hospl_ch5_membugs/membugs.c
@@ -26,14 +26,49 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include "../common.h"
+#include <signal.h>
+#include <stdarg.h>
+#include <errno.h>
 
-/*---------------- Globals, Macros ----------------------------*/
+int handle_err(int fatal, const char *fmt, ...)
+{
+#define ERRSTRMAX 512
+	char *err_str;
+	va_list argp;
+
+	err_str = malloc(ERRSTRMAX);
+	if (err_str == NULL)
+		return -1;
+
+	va_start(argp, fmt);
+	vsnprintf(err_str, ERRSTRMAX-1, fmt, argp);
+	va_end(argp);
+
+	fprintf(stderr, "%s", err_str);
+	if (errno) {
+		fprintf(stderr, "  ");
+		perror("kernel says");
+	}
+
+	free(err_str);
+	if (!fatal)
+		return 0;
+	exit(fatal);
+}
+
+/*--- Macros ---*/
+#define NON_FATAL    0
+
+#define WARN(warnmsg, args...) do {                           \
+	handle_err(NON_FATAL, "!WARNING! %s:%s:%d: " warnmsg, \
+	   __FILE__, __func__, __LINE__, ##args);         \
+} while(0)
+#define FATAL(errmsg, args...) do {                           \
+	handle_err(EXIT_FAILURE, "FATAL:%s:%s:%d: " errmsg,   \
+	   __FILE__, __func__, __LINE__, ##args);         \
+} while(0)
+
 static const size_t BLK_1MB = 1024*1024;
-
-/*---------------- Typedef's, constants, etc ------------------*/
-
-/*---------------- Functions ----------------------------------*/
 
 /*
  * A demo: this function allocates memory internally; the caller
